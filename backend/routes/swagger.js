2bbea47dcbd0b8d32ba6faa7b4afb86a709fe7b4
klee2187 <kkelly6@byui.edu>
2026-03-02 21:55:20 -0700

    added to swagger.js

--- backend/routes/swagger.js
+++ backend/routes/swagger.js
@@ -4,5 +4,9 @@ const swaggerDocument = require('../swagger.json');
  
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+router.get('/api-docs/swagger.json', (req, res) => {
+    res.json(swaggerDocument);
+});
  
 module.exports = router;
