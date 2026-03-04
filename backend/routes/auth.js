7225415f3a21bcd965ba1bce5fffbb0152415f87
klee2187 <kkelly6@byui.edu>
2026-03-04 02:02:08 -0700

    trying to fix add button

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -26,9 +26,9 @@ router.get('/google/callback',
             );
 
             res.cookie('swagger_token', token, {
-            httpOnly: true,
-            secure: true,
-            sameSite: "none",
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? "none" : "lax",
         });
         
         res.redirect('/dashboard');
